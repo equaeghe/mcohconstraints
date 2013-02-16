1cb41529471ca8ca23cf8d679534a8b2f74368e1
Erik Quaeghebeur <Erik.Quaeghebeur@UGent.be>
2013-02-16 21:21:11 +0100

    allow the MOLP natex and cohcorr functions to ingest 'free' type constraints

--- cohcorr_bensolve.m
+++ cohcorr_bensolve.m
@@ -1,11 +1,14 @@
 function [cohcorr, maxnum] = cohcorr_bensolve(constraints, lpr)
 
   m = length(lpr);
+  k = size(constraints.A, 2);
 
-  [~, ~, ~, cohcorr_maximals, ~, ~] = bensolve(-eye(m), ...
-                                               [-constraints.A; -eye(m)], ...
-                                               [-constraints.B; -lpr], ...
-                                               [], [], [], struct('info', 0));
+  C = [-eye(m), zeros(m, k-m)];
+  A = [-constraints.A; -eye(m), zeros(m, k-m)];
+  b = [-constraints.B; -lpr];
+
+  [~, ~, ~, cohcorr_maximals, ~, ~] = bensolve(C, A, b, [], [], [], ...
+                                               struct('info', 0));
 
   cohcorr = min(-cohcorr_maximals')';
   maxnum = size(cohcorr_maximals, 2);
--- natex_bensolve.m
+++ natex_bensolve.m
@@ -1,9 +1,12 @@
 function natex = natex_bensolve(constraints, lpr)
 
-  m = length(lpr)
+  m = length(lpr);
+  k = size(constraints.A, 2);
 
-  [~, ~, ~, natex, ~, ~] = bensolve(eye(m), [-constraints.A; eye(m)], ...
-                                            [-constraints.B; lpr], ...
-                                    [], [], [], struct('info', 0));
+  C = [eye(m), zeros(m, k-m)];
+  A = [-constraints.A; eye(m), zeros(m, k-m)];
+  b = [-constraints.B; lpr];
+
+  [~, ~, ~, natex, ~, ~] = bensolve(C, A, b, [], [], [], struct('info', 0));
 
 end
