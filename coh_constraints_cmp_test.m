9581f78c767179a1bafb9ffcdd50ef1639d5f99d
Erik Quaeghebeur <Erik.Quaeghebeur@UGent.be>
2013-04-14 10:49:18 +0200

    do not run MOLP coh_constraints approach (too slow) and sidestep numerical instabilities

--- coh_constraints_cmp_test.m
+++ coh_constraints_cmp_test.m
@@ -1,37 +1,44 @@
 function [time_vertrays, ...
           time_cddmex, ...
-          time_bensolve] = coh_constraints_cmp_test(statenum, ...
-                                                    gamblenum, trials)
+          mistrials] = coh_constraints_cmp_test(statenum, gamblenum, trials)
 
   % initialize
   time_vertrays = 0;
   time_cddmex = 0;
-  time_bensolve = 0;
+%  time_bensolve = 0;
 
-  for k = 1:trials
-    % generate random K
-    Kevents = floor(rand(statenum, gamblenum) + rand);
-    Kvals = floor(10 * rand(statenum, gamblenum));
-    K = Kevents .* Kvals;
+  k = 0;
+  mistrials = 0;
+  do
+    try
+      % generate random K
+      Kevents = floor(rand(statenum, gamblenum) + rand);
+      Kvals = floor(10 * rand(statenum, gamblenum));
+      Kpos = Kevents .* Kvals;
+      K = Kpos - ones(statenum, 1) * min(Kpos);
 
-    start = cputime
-      coh_constraints_vertrays(K)
-    stop = cputime
-    time_vertrays = time_vertrays + stop - start
+      start = cputime;
+        coh_constraints_vertrays(K);
+      stop = cputime;
+      time_vertrays = time_vertrays + stop - start;
 
-    start = cputime
-      coh_constraints_cddmex(K)
-    stop = cputime
-    time_cddmex = time_cddmex + stop - start
+      start = cputime;
+        coh_constraints_cddmex(K);
+      stop = cputime;
+      time_cddmex = time_cddmex + stop - start;
 
-    start = cputime
-      coh_constraints_bensolve(K)
-    stop = cputime
-    time_bensolve = time_bensolve + stop - start
-  end
+  %    start = cputime;
+  %      coh_constraints_bensolve(K);
+  %    stop = cputime;
+  %    time_bensolve = time_bensolve + stop - start;
+      k += 1;
+    catch
+      mistrials += 1;
+    end_try_catch
+  until k == trials
 
   time_vertrays = time_vertrays / trials;
   time_cddmex = time_cddmex / trials;
-  time_bensolve = time_bensolve / trials;
+%  time_bensolve = time_bensolve / trials;
 
 end
